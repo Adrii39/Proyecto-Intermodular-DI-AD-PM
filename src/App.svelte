bee1c97c287fb97677bb7864ca5aee8e9095d7ab
Adrian8 <acatalans@alumnos.nebrija.es>
2025-11-24 11:06:48 +0100

    Importo el Grafico en app.svelte; creo el componente con chart.js y muestro un gráfico de prueba

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import GraficoBurbujas from "./lib/GraficoBurbujas.svelte";
+</script>
+
+<main style="text-align: center;">
+  <h1>Primer gráfico</h1>
+  <GraficoBurbujas />
+</main>
---
+++ src/lib/GraficoBurbujas.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { onMount } from "svelte";
+    import { Chart } from "chart.js/auto";
+
+
+    let graficoBurbujas;
+    let chart;
+
+    onMount(() => {
+        chart = new Chart (graficoBurbujas, {
+            type: 'bubble',
+            data: {
+                datasets: [
+                    {
+                        label: "Prueba",
+                        data: [{ x: 10, y: 20, r: 5}],
+                        backgroundColor: "red"
+                    }
+
+                ]},
+            options: {
+                responsive: false,
+                plugins: {
+                    legend: { position: 'top'},
+                    title: { display: true, text: 'Curva de movilidad intergeneracional' }
+                }
+            }
+        });
+    });
+
+</script>
+
+<div style="text-align: center;">
+    <canvas bind:this={graficoBurbujas} width="600" height="500"></canvas>
+
+</div>
