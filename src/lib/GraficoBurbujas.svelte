be3c75ea2826f88b1140b8b9a3c7b30d907da311
Adrian8 <acatalans@alumnos.nebrija.es>
2025-11-25 13:42:09 +0100

    Añado un filtro interactivo y una función para actualizar el gráfico

--- src/lib/GraficoBurbujas.svelte
+++ src/lib/GraficoBurbujas.svelte
@@ -6,6 +6,32 @@
     let graficoBurbujas;
     let chart;
     let datosBurbujas = [];
+    let filtroBurbuja = "meidana";
+
+    function actualizarGraficoBurbuja() {
+
+        if (!chart) return;
+
+        const puntos = datosBurbujas
+        .filter(r => r[3] === filtroBurbuja)
+        .map(r => ({
+            x: parseInt(r[2]),
+            y: parseFloat(r[5]).toFixed(1),
+            r: 8
+        }));
+
+        chart.data.datasets = [{
+            label: 'Movilidad Intergeneracional',
+            data: puntos,
+            fill: false,
+            borderColor: "lightblue",
+            backgroundColor: "black",
+            tension: 0.3,
+            pointRadius: 5
+        }];
+
+        chart.update();
+    }
 
     onMount(async() => {
 
@@ -16,27 +42,38 @@
 
         chart = new Chart (graficoBurbujas, {
             type: 'bubble',
-            data: {
-                datasets: [
-                    {
-                        label: "Prueba",
-                        data: [{ x: 10, y: 20, r: 5}],
-                        backgroundColor: "red"
-                    }
-
-                ]},
+            data: { datasets: [] },
             options: {
                 responsive: false,
                 plugins: {
-                    legend: { position: 'top'},
-                    title: { display: true, text: 'Curva de movilidad intergeneracional' }
+                    legend: { position: 'top', onClick: null},
+                    title: { display: true, text: 'Curva de movilidad intergeneracional' },
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                const point = context.raw;
+                                return `Padres: ${point['x']}, Hijos: ${point['y']}`;
+
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    x: { type: 'linear', title: { display:true, text: 'Centil de padres' }, min: 0, max: 100},
+                    y: { title: { display: true, text: 'Centil de hijos' }, min: 0, max: 100 }
                 }
             }
         });
+
+        actualizarGraficoBurbuja();
     });
 
 </script>
 
 <div style="text-align: center;">
+    <select bind:value={filtroBurbuja} on:change={actualizarGraficoBurbuja}>
+        <option value="mediana">Mediana</option>
+        <option value="media">media</option>
+    </select>
     <canvas bind:this={graficoBurbujas} width="600" height="500"></canvas>
 </div>
